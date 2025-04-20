75beed2477a8bdd4efcf7f967ca068443ed0ee57
Preethi Kamal Gajula <145118298+preethikamal@users.noreply.github.com>
2025-04-20 21:02:13 +0530

    Create Cart.java

---
+++ src/Cart.java
@@ -0,0 +1,212 @@
+/**
+ * 
+ */
+
+import httpwww.w3.org2001XMLSchema._float;
+
+/** 
+ * @author STUDENT
+ * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+ */
+public class Cart {
+    /** 
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    private Integer orderid;
+
+    /** 
+     * @return the orderid
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    public Integer getOrderid() {
+        // begin-user-code
+        return orderid;
+        // end-user-code
+    }
+
+    /** 
+     * @param theOrderid the orderid to set
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    public void setOrderid(Integer theOrderid) {
+        // begin-user-code
+        orderid = theOrderid;
+        // end-user-code
+    }
+
+    /** 
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    private _float totalAmt;
+
+    /** 
+     * @return the totalAmt
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    public _float getTotalAmt() {
+        // begin-user-code
+        return totalAmt;
+        // end-user-code
+    }
+
+    /** 
+     * @param theTotalAmt the totalAmt to set
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    public void setTotalAmt(_float theTotalAmt) {
+        // begin-user-code
+        totalAmt = theTotalAmt;
+        // end-user-code
+    }
+
+    /** 
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    private String offer;
+
+    /** 
+     * @return the offer
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    public String getOffer() {
+        // begin-user-code
+        return offer;
+        // end-user-code
+    }
+
+    /** 
+     * @param theOffer the offer to set
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    public void setOffer(String theOffer) {
+        // begin-user-code
+        offer = theOffer;
+        // end-user-code
+    }
+
+    /** 
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    private Integer quantity;
+
+    /** 
+     * @return the quantity
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    public Integer getQuantity() {
+        // begin-user-code
+        return quantity;
+        // end-user-code
+    }
+
+    /** 
+     * @param theQuantity the quantity to set
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    public void setQuantity(Integer theQuantity) {
+        // begin-user-code
+        quantity = theQuantity;
+        // end-user-code
+    }
+
+    /** 
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    private Integer itemid;
+
+    /** 
+     * @return the itemid
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    public Integer getItemid() {
+        // begin-user-code
+        return itemid;
+        // end-user-code
+    }
+
+    /** 
+     * @param theItemid the itemid to set
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    public void setItemid(Integer theItemid) {
+        // begin-user-code
+        itemid = theItemid;
+        // end-user-code
+    }
+
+    /** 
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    private Integer userid;
+
+    /** 
+     * @return the userid
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    public Integer getUserid() {
+        // begin-user-code
+        return userid;
+        // end-user-code
+    }
+
+    /** 
+     * @param theUserid the userid to set
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    public void setUserid(Integer theUserid) {
+        // begin-user-code
+        userid = theUserid;
+        // end-user-code
+    }
+
+    /** 
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    private User user;
+
+    /** 
+     * @return the user
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    public User getUser() {
+        // begin-user-code
+        return user;
+        // end-user-code
+    }
+
+    /** 
+     * @param theUser the user to set
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    public void setUser(User theUser) {
+        // begin-user-code
+        user = theUser;
+        // end-user-code
+    }
+
+    /** 
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    private Menu menu;
+
+    /** 
+     * @return the menu
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    public Menu getMenu() {
+        // begin-user-code
+        return menu;
+        // end-user-code
+    }
+
+    /** 
+     * @param theMenu the menu to set
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    public void setMenu(Menu theMenu) {
+        // begin-user-code
+        menu = theMenu;
+        // end-user-code
+    }
+}
+
