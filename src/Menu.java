372ce958b1b8da7e08693d5bfb6efe774157fcfc
Preethi Kamal Gajula <145118298+preethikamal@users.noreply.github.com>
2025-04-20 21:07:08 +0530

    Create Menu.java

---
+++ src/Menu.java
@@ -0,0 +1,219 @@
+/**
+ * 
+ */
+
+import java.util.Set;
+
+/** 
+ * @author STUDENT
+ * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+ */
+public class Menu {
+    /** 
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    private Integer itemid;
+
+    /** 
+     * @return the itemid
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    public Integer getItemid() {
+        // begin-user-code
+        return itemid;
+        // end-user-code
+    }
+
+    /** 
+     * @param theItemid the itemid to set
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    public void setItemid(Integer theItemid) {
+        // begin-user-code
+        itemid = theItemid;
+        // end-user-code
+    }
+
+    /** 
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    private Integer restaurantid;
+
+    /** 
+     * @return the restaurantid
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    public Integer getRestaurantid() {
+        // begin-user-code
+        return restaurantid;
+        // end-user-code
+    }
+
+    /** 
+     * @param theRestaurantid the restaurantid to set
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    public void setRestaurantid(Integer theRestaurantid) {
+        // begin-user-code
+        restaurantid = theRestaurantid;
+        // end-user-code
+    }
+
+    /** 
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    private String itemname;
+
+    /** 
+     * @return the itemname
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    public String getItemname() {
+        // begin-user-code
+        return itemname;
+        // end-user-code
+    }
+
+    /** 
+     * @param theItemname the itemname to set
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    public void setItemname(String theItemname) {
+        // begin-user-code
+        itemname = theItemname;
+        // end-user-code
+    }
+
+    /** 
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    private Integer price;
+
+    /** 
+     * @return the price
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    public Integer getPrice() {
+        // begin-user-code
+        return price;
+        // end-user-code
+    }
+
+    /** 
+     * @param thePrice the price to set
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    public void setPrice(Integer thePrice) {
+        // begin-user-code
+        price = thePrice;
+        // end-user-code
+    }
+
+    /** 
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    private Boolean availability;
+
+    /** 
+     * @return the availability
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    public Boolean getAvailability() {
+        // begin-user-code
+        return availability;
+        // end-user-code
+    }
+
+    /** 
+     * @param theAvailability the availability to set
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    public void setAvailability(Boolean theAvailability) {
+        // begin-user-code
+        availability = theAvailability;
+        // end-user-code
+    }
+
+    /** 
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    private Cart cart;
+
+    /** 
+     * @return the cart
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    public Cart getCart() {
+        // begin-user-code
+        return cart;
+        // end-user-code
+    }
+
+    /** 
+     * @param theCart the cart to set
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    public void setCart(Cart theCart) {
+        // begin-user-code
+        cart = theCart;
+        // end-user-code
+    }
+
+    /** 
+     * @uml.annotations for <code>cart2</code>
+     *     collection_type="Cart"
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    private Set<Cart> cart2;
+
+    /** 
+     * @return the cart2
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    public Set<Cart> getCart2() {
+        // begin-user-code
+        return cart2;
+        // end-user-code
+    }
+
+    /** 
+     * @param theCart2 the cart2 to set
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    public void setCart2(Set<Cart> theCart2) {
+        // begin-user-code
+        cart2 = theCart2;
+        // end-user-code
+    }
+
+    /** 
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    public void addItem() {
+        // begin-user-code
+        // TODO Auto-generated method stub
+
+        // end-user-code
+    }
+
+    /** 
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    public void removeItem() {
+        // begin-user-code
+        // TODO Auto-generated method stub
+
+        // end-user-code
+    }
+
+    /** 
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    public void updateItem() {
+        // begin-user-code
+        // TODO Auto-generated method stub
+
+        // end-user-code
+    }
+}
+
