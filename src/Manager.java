bde7cf73eb214195ec26c2cebafee324c7c9964b
Preethi Kamal Gajula <145118298+preethikamal@users.noreply.github.com>
2025-04-20 21:06:08 +0530

    Create Manager.java

---
+++ src/Manager.java
@@ -0,0 +1,187 @@
+/**
+ * 
+ */
+
+import httpwww.w3.org2001XMLSchema._float;
+import java.util.Set;
+
+/** 
+ * @author STUDENT
+ * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+ */
+public class Manager {
+    /** 
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    private _float rating;
+
+    /** 
+     * @return the rating
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    public _float getRating() {
+        // begin-user-code
+        return rating;
+        // end-user-code
+    }
+
+    /** 
+     * @param theRating the rating to set
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    public void setRating(_float theRating) {
+        // begin-user-code
+        rating = theRating;
+        // end-user-code
+    }
+
+    /** 
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    private String location;
+
+    /** 
+     * @return the location
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    public String getLocation() {
+        // begin-user-code
+        return location;
+        // end-user-code
+    }
+
+    /** 
+     * @param theLocation the location to set
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    public void setLocation(String theLocation) {
+        // begin-user-code
+        location = theLocation;
+        // end-user-code
+    }
+
+    /** 
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    private String name;
+
+    /** 
+     * @return the name
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    public String getName() {
+        // begin-user-code
+        return name;
+        // end-user-code
+    }
+
+    /** 
+     * @param theName the name to set
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    public void setName(String theName) {
+        // begin-user-code
+        name = theName;
+        // end-user-code
+    }
+
+    /** 
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    private Integer retaurantid;
+
+    /** 
+     * @return the retaurantid
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    public Integer getRetaurantid() {
+        // begin-user-code
+        return retaurantid;
+        // end-user-code
+    }
+
+    /** 
+     * @param theRetaurantid the retaurantid to set
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    public void setRetaurantid(Integer theRetaurantid) {
+        // begin-user-code
+        retaurantid = theRetaurantid;
+        // end-user-code
+    }
+
+    /** 
+     * @uml.annotations for <code>menu</code>
+     *     collection_type="Menu"
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    private Set<Menu> menu;
+
+    /** 
+     * @return the menu
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    public Set<Menu> getMenu() {
+        // begin-user-code
+        return menu;
+        // end-user-code
+    }
+
+    /** 
+     * @param theMenu the menu to set
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    public void setMenu(Set<Menu> theMenu) {
+        // begin-user-code
+        menu = theMenu;
+        // end-user-code
+    }
+
+    /** 
+     * @uml.annotations for <code>deliveryperson</code>
+     *     collection_type="DeliveryPerson"
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    private Set<DeliveryPerson> deliveryperson;
+
+    /** 
+     * @return the deliveryperson
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    public Set<DeliveryPerson> getDeliveryperson() {
+        // begin-user-code
+        return deliveryperson;
+        // end-user-code
+    }
+
+    /** 
+     * @param theDeliveryperson the deliveryperson to set
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    public void setDeliveryperson(Set<DeliveryPerson> theDeliveryperson) {
+        // begin-user-code
+        deliveryperson = theDeliveryperson;
+        // end-user-code
+    }
+
+    /** 
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    public void acceptOrder() {
+        // begin-user-code
+        // TODO Auto-generated method stub
+
+        // end-user-code
+    }
+
+    /** 
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    public void manageMenu() {
+        // begin-user-code
+        // TODO Auto-generated method stub
+
+        // end-user-code
+    }
+}
+
