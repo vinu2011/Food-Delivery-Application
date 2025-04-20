3422414e7eaff392440f259df1b0e5af1308c814
Preethi Kamal Gajula <145118298+preethikamal@users.noreply.github.com>
2025-04-20 21:03:10 +0530

    Create Deliveryorders.java

---
+++ src/Deliveryorders.java
@@ -0,0 +1,242 @@
+/**
+ * 
+ */
+
+import httpwww.w3.org2001XMLSchema._float;
+import java.util.Set;
+
+/** 
+ * @author STUDENT
+ * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+ */
+public class DeliveryOrders {
+    /** 
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    private Integer orderamt;
+
+    /** 
+     * @return the orderamt
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    public Integer getOrderamt() {
+        // begin-user-code
+        return orderamt;
+        // end-user-code
+    }
+
+    /** 
+     * @param theOrderamt the orderamt to set
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    public void setOrderamt(Integer theOrderamt) {
+        // begin-user-code
+        orderamt = theOrderamt;
+        // end-user-code
+    }
+
+    /** 
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    private Integer paymentid;
+
+    /** 
+     * @return the paymentid
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    public Integer getPaymentid() {
+        // begin-user-code
+        return paymentid;
+        // end-user-code
+    }
+
+    /** 
+     * @param thePaymentid the paymentid to set
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    public void setPaymentid(Integer thePaymentid) {
+        // begin-user-code
+        paymentid = thePaymentid;
+        // end-user-code
+    }
+
+    /** 
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    private _float rating;
+
+    /** 
+     * @return the rating
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    public _float getRating() {
+        // begin-user-code
+        return rating;
+        // end-user-code
+    }
+
+    /** 
+     * @param theRating the rating to set
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    public void setRating(_float theRating) {
+        // begin-user-code
+        rating = theRating;
+        // end-user-code
+    }
+
+    /** 
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    private Integer assignedDeliveryPersonid;
+
+    /** 
+     * @return the assignedDeliveryPersonid
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    public Integer getAssignedDeliveryPersonid() {
+        // begin-user-code
+        return assignedDeliveryPersonid;
+        // end-user-code
+    }
+
+    /** 
+     * @param theAssignedDeliveryPersonid the assignedDeliveryPersonid to set
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    public void setAssignedDeliveryPersonid(Integer theAssignedDeliveryPersonid) {
+        // begin-user-code
+        assignedDeliveryPersonid = theAssignedDeliveryPersonid;
+        // end-user-code
+    }
+
+    /** 
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    private Integer orderid;
+
+    /** 
+     * @return the orderid
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    public Integer getOrderid() {
+        // begin-user-code
+        return orderid;
+        // end-user-code
+    }
+
+    /** 
+     * @param theOrderid the orderid to set
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    public void setOrderid(Integer theOrderid) {
+        // begin-user-code
+        orderid = theOrderid;
+        // end-user-code
+    }
+
+    /** 
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    private Integer cartid;
+
+    /** 
+     * @return the cartid
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    public Integer getCartid() {
+        // begin-user-code
+        return cartid;
+        // end-user-code
+    }
+
+    /** 
+     * @param theCartid the cartid to set
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    public void setCartid(Integer theCartid) {
+        // begin-user-code
+        cartid = theCartid;
+        // end-user-code
+    }
+
+    /** 
+     * @uml.annotations for <code>deliveryperson</code>
+     *     collection_type="DeliveryPerson"
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    private Set<DeliveryPerson> deliveryperson;
+
+    /** 
+     * @return the deliveryperson
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    public Set<DeliveryPerson> getDeliveryperson() {
+        // begin-user-code
+        return deliveryperson;
+        // end-user-code
+    }
+
+    /** 
+     * @param theDeliveryperson the deliveryperson to set
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    public void setDeliveryperson(Set<DeliveryPerson> theDeliveryperson) {
+        // begin-user-code
+        deliveryperson = theDeliveryperson;
+        // end-user-code
+    }
+
+    /** 
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    private DeliveryPerson deliveryperson2;
+
+    /** 
+     * @return the deliveryperson2
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    public DeliveryPerson getDeliveryperson2() {
+        // begin-user-code
+        return deliveryperson2;
+        // end-user-code
+    }
+
+    /** 
+     * @param theDeliveryperson2 the deliveryperson2 to set
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    public void setDeliveryperson2(DeliveryPerson theDeliveryperson2) {
+        // begin-user-code
+        deliveryperson2 = theDeliveryperson2;
+        // end-user-code
+    }
+
+    /** 
+     * @uml.annotations for <code>user</code>
+     *     collection_type="User"
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    private Set<User> user;
+
+    /** 
+     * @return the user
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    public Set<User> getUser() {
+        // begin-user-code
+        return user;
+        // end-user-code
+    }
+
+    /** 
+     * @param theUser the user to set
+     * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+     */
+    public void setUser(Set<User> theUser) {
+        // begin-user-code
+        user = theUser;
+        // end-user-code
+    }
+}
+
